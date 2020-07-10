c4b97baf2b30b30992a32fe86480d9ceff200297
Matej Drzik <matej.drzik@quanto.sk>
2020-07-10 18:18:02 +0200

    ui updates backport

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,64 +1,121 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { Layout, Card, Spinner, Button, Icon } from '@ui-kitten/components';
-import ProximiioMapbox, { ProximiioMapboxEvents } from 'react-native-proximiio-mapbox';
-import { ProximiioMapboxRouteUpdateEvent, ProximiioMapboxRouteUpdate } from 'react-native-proximiio-mapbox/lib/typescript/src/types';
+import { Layout, Card, Button } from '@ui-kitten/components';
+import ProximiioMapbox from 'react-native-proximiio-mapbox';
+import { ProximiioRouteEvents } from '../../node_modules/react-native-proximiio-mapbox/src/route_manager';
 
 export interface Props {
+  previewMode: boolean;
+  onCancel: () => void;
 }
 
 export interface State {
-  instruction?: string
+  instruction?: string;
+  distance?: number;
+  duration?: number;
 }
 
 export default class Instructions extends React.Component<Props, State> {
-  state = {} as State
+  state = {} as State;
 
   componentDidMount() {
-    ProximiioMapbox.subscribe(ProximiioMapboxEvents.ROUTE_STARTED, this.start)
-    ProximiioMapbox.subscribe(ProximiioMapboxEvents.ROUTE_UPDATED, this.update)
-    ProximiioMapbox.subscribe(ProximiioMapboxEvents.ROUTE_CANCELED, this.cancel)
+    ProximiioMapbox.route.on(this.onRouteEvent);
   }
 
   componentWillUnmount() {
-    ProximiioMapbox.unsubscribe(ProximiioMapboxEvents.ROUTE_STARTED, this.start)
-    ProximiioMapbox.unsubscribe(ProximiioMapboxEvents.ROUTE_UPDATED, this.update)
-    ProximiioMapbox.unsubscribe(ProximiioMapboxEvents.ROUTE_CANCELED, this.cancel)
+    ProximiioMapbox.route.off(this.onRouteEvent);
   }
 
-  private start = () => this.setState({ instruction: 'Route starting' })
+  private onRouteEvent = (event?: string, data?: any) => {
+    if (event === ProximiioRouteEvents.ROUTE_STARTED) {
+      this.start();
+      return;
+    }
 
-  private cancel = () => this.setState({ instruction: undefined})
+    if (event === ProximiioRouteEvents.ROUTE_UPDATED) {
+      this.update();
+      return;
+    }
 
-  private update = async (evt: any) => {
-    console.log('route update', evt)
-    this.setState({ instruction: evt.text })
-  }
+    if (event === ProximiioRouteEvents.ROUTE_CANCELED) {
+      this.cancel();
+    }
+  };
+
+  private start = () => {
+    this.update();
+  };
+
+  private cancel = () => {
+    this.setState({ instruction: undefined });
+    this.props.onCancel();
+  };
+
+  private update = async () => {
+    const { route } = ProximiioMapbox.route;
+    if (route && route.descriptor.steps.length > 1) {
+      this.setState({
+        instruction: ProximiioMapbox.route.isPreview
+          ? 'Route preview only mode'
+          : route.descriptor.steps[1].instruction,
+        distance: route.descriptor.distanceMeters,
+        duration: route.descriptor.duration,
+      });
+    }
+  };
 
   public render() {
     if (!this.state.instruction) {
-      return null
+      return null;
     }
 
-    return <Layout
-      style={{
-        position: 'absolute',
-        alignSelf: 'center',
-        bottom: 50,
-        width: '100%'
-      }}>
-      <Card status='control'>
-        <Layout style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <Text style={{ marginRight: 20, flex: 1 }}>{ this.state.instruction }</Text>
-          <Button 
-            size="small" 
-            appearance="outline" 
-            onPress={() => ProximiioMapbox.route.cancel()} 
-            style={{ alignSelf: 'flex-end', margin: 0 }}>
-            Stop Navigation
-          </Button>
-        </Layout>
-      </Card>
-    </Layout>
+    return (
+      <Layout
+        style={{
+          position: 'absolute',
+          alignSelf: 'center',
+          bottom: 50,
+          width: "100%",
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+        }}
+      >
+        <Card status="control">
+          <Layout
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ marginRight: 20, flex: 1 }}>
+              {this.state.instruction}
+            </Text>
+            <Button
+              size="small"
+              appearance="outline"
+              onPress={() => ProximiioMapbox.route.cancel()}
+              style={{ alignSelf: 'flex-end', margin: 0 }}
+            >
+              {this.props.previewMode ? 'Cancel Preview' : 'Stop Navigation'}
+            </Button>
+          </Layout>
+          {ProximiioMapbox.route.isStarted && (
+            <Layout
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={{ marginRight: 20, flex: 1 }}>
+                Distance: {this.state.distance?.toFixed(0)} meters (
+                {this.state.duration?.toFixed(0)} seconds)
+              </Text>
+            </Layout>
+          )}
+        </Card>
+      </Layout>
+    );
   }
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,136 +1,148 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import MapView from '../components/MapView'
-import PositioningStatus from '../components/PositioningStatus'
-import settings from '../../settings'
-import Proximiio, { ProximiioLocation } from 'react-native-proximiio'
-import { column } from '../styles'
-import LoadingView from '../components/LoadingView'
-import SyncView from '../components/SyncView'
-import SearchBar from '../components/SearchBar'
-import SearchList from '../components/SearchList'
-import Instructions from '../components/Instructions'
-import { ProximiioContextProvider } from 'react-native-proximiio'
-import ProximiioMapbox, { Feature, FeatureCollection } from 'react-native-proximiio-mapbox'
+import React, { Component } from "react";
+import { View } from "react-native";
+import MapView from "../components/MapView";
+import PositioningStatus from "../components/PositioningStatus";
+import settings from "../../settings";
+import Proximiio, {
+  ProximiioLocation,
+  ProximiioEvents,
+} from "react-native-proximiio";
+import { column } from "../styles";
+import LoadingView from "../components/LoadingView";
+import SyncView from "../components/SyncView";
+import SearchBar from "../components/SearchBar";
+import SearchList from "../components/SearchList";
+import Instructions from "../components/Instructions";
+import { ProximiioContextProvider } from "react-native-proximiio";
+import ProximiioMapbox, {
+  Feature,
+  FeatureCollection,
+} from "react-native-proximiio-mapbox";
 
-interface Props {
-}
+interface Props {}
 
 interface State {
-  collection: FeatureCollection
-  points: FeatureCollection
-  location?: ProximiioLocation
-  initialized: boolean
-  centerCoordinate: number[]
-  animationDuration: number
-  zoom: number
-  menuVisible: boolean
-  syncing: boolean
-  query: string
-  level: number
-  title: string
+  collection: FeatureCollection;
+  points: FeatureCollection;
+  location?: ProximiioLocation;
+  initialized: boolean;
+  centerCoordinate: number[];
+  animationDuration: number;
+  zoom: number;
+  menuVisible: boolean;
+  syncing: boolean;
+  query: string;
+  level: number;
+  title: string;
 }
 
 class Main extends Component<Props, State> {
   constructor(props: Props) {
-    super(props)
+    super(props);
     this.state = {
-      collection: { type: 'FeatureCollection', features: [] },
-      points: { type: 'FeatureCollection', features: [] },
+      collection: { type: "FeatureCollection", features: [] },
+      points: { type: "FeatureCollection", features: [] },
       initialized: false,
       syncing: false,
       centerCoordinate: settings.map.coordinates,
       zoom: settings.map.zoom,
       animationDuration: settings.map.animation.duration,
       menuVisible: false,
-      query: '',
+      query: "",
       level: 0,
-      title: 'Map / Navigation'
-    }
+      title: "Map / Navigation",
+    };
 
-    this.toggleMenu = this.toggleMenu.bind(this)
-    this.onMenuItemSelect = this.onMenuItemSelect.bind(this)
-    this.onSearchSelect = this.onSearchSelect.bind(this)
-    this.onQueryChange = this.onQueryChange.bind(this)
-    this.onLevelChange = this.onLevelChange.bind(this)
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.onMenuItemSelect = this.onMenuItemSelect.bind(this);
+    this.onSearchSelect = this.onSearchSelect.bind(this);
+    this.onQueryChange = this.onQueryChange.bind(this);
+    this.onLevelChange = this.onLevelChange.bind(this);
   }
 
   componentDidMount() {
-    this.initialize()
+    this.initialize();
   }
 
   async initialize() {
-    await Proximiio.authorize(settings.proximiio.token)
-    await ProximiioMapbox.authorize(settings.proximiio.token)
-
-    Proximiio.requestPermissions()
+    await Proximiio.authorize(settings.proximiio.token);
+    await ProximiioMapbox.authorize(settings.proximiio.token);
+    Proximiio.subscribe(ProximiioEvents.UpdatedIBeacon, (beacon) =>
+      console.log("beacon update", beacon)
+    );
+    Proximiio.requestPermissions();
     await this.setState({
-      initialized: true
-    })
+      initialized: true,
+    });
   }
 
-  toggleMenu () {
-    this.setState({ menuVisible: !this.state.menuVisible })
+  toggleMenu() {
+    this.setState({ menuVisible: !this.state.menuVisible });
   }
 
   onLevelChange(level: number) {
-    this.setState({ level })
+    this.setState({ level });
   }
 
   onQueryChange(query: string) {
-    this.setState({ query })
+    this.setState({ query });
   }
 
   onSearchSelect(feature: Feature) {
     ProximiioMapbox.route.find(feature.id, false);
-    this.setState({ query: '', })
+    this.setState({ query: "" });
   }
 
-  onMenuItemSelect (index: number) {
-    this.setState({ menuVisible: false })
+  onMenuItemSelect(_: number) {
+    this.setState({ menuVisible: false });
+  }
+
+  cancelRoute = () => {
+    ProximiioMapbox.route.cancel();
   }
 
   renderFloating() {
-    return <React.Fragment>
-      < PositioningStatus />
-      { this.state.syncing && <SyncView /> }
-      
-      <SearchList
-        level={this.state.level}
-        query={this.state.query}
-        onSelect={this.onSearchSelect}
-      />
+    return (
+      <React.Fragment>
+        <PositioningStatus />
+        {this.state.syncing && <SyncView />}
 
-      <SearchBar 
-        query={this.state.query} 
-        onChange={this.onQueryChange}
-      />
-      
-      <Instructions />
-    </React.Fragment>
+        <SearchList
+          level={this.state.level}
+          query={this.state.query}
+          onSelect={this.onSearchSelect}
+        />
+
+        <SearchBar query={this.state.query} onChange={this.onQueryChange} />
+
+        <Instructions onCancel={this.cancelRoute} previewMode={false} />
+      </React.Fragment>
+    );
   }
 
   render() {
     if (!this.state.initialized) {
-      return <LoadingView />
+      return <LoadingView />;
     }
 
-    return <View style={column}>
-      <ProximiioContextProvider>
-        <MapView
-          collection={this.state.collection}
-          points={this.state.points}
-          centerCoordinate={this.state.centerCoordinate}
-          level={this.state.level}
-          zoom={this.state.zoom}
-          userLocation={this.state.location}
-          animationDuration={this.state.animationDuration}
-          onLevelChange={this.onLevelChange}
-        />
-        { this.renderFloating() }
-      </ProximiioContextProvider>
-    </View>
+    return (
+      <View style={column}>
+        <ProximiioContextProvider>
+          <MapView
+            collection={this.state.collection}
+            points={this.state.points}
+            centerCoordinate={this.state.centerCoordinate}
+            level={this.state.level}
+            zoom={this.state.zoom}
+            userLocation={this.state.location}
+            animationDuration={this.state.animationDuration}
+            onLevelChange={this.onLevelChange}
+          />
+          {this.renderFloating()}
+        </ProximiioContextProvider>
+      </View>
+    );
   }
 }
 
-export default Main
+export default Main;
